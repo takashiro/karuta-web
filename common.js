a0e29f39c9f68c8b75d77dd6394824fa674c7440
Takashiro <takashiro@qq.com>
2017-05-25 01:27:30 +0800

    add function httpGet()

--- common.js
+++ common.js
@@ -15,3 +15,19 @@ function enums(options){
 	}
 	return object;
 }
+
+function httpGet(){
+	var i = location.href.indexOf('?');
+	if(i <= 0){
+		return {};
+	}
+	var query_string = location.href.substr(i + 1).split('&');
+	var result  = {};
+	for(var i = 0; i < query_string.length; i++){
+		var pair = query_string[i].split('=');
+		if(pair.length > 1){
+			result[pair[0]] = pair[1];
+		}
+	}
+	return result;
+}
